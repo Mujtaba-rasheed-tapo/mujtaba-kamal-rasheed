59e7f797cb41d11c9541cc4b25a7f5c48dcb9704
Mujtaba-rasheed-tapo <74573698+Mujtaba-rasheed-tapo@users.noreply.github.com>
2020-11-19 06:58:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<! DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+        <meta name="viewpor" content=" width=device-width, initial-scale=1.8">
+        <title>البيانات الاساسية</title>
+    </head>
+<body bgcolor="#00000">
+          
+              
+              <font color='white'>
+                  
+                      <center>
+    <h1> بسـم الله الـرحمن الـرحيم </h1>
+
+    <h2>كلية الأمـارات للعلوم والتكنولوجيــا</h2>
+    <h2>قســم : تقنيـة المعلومــات</h2>
+    <h2>(( البيانــات الأســـــاسية ))</h2>
+    <h2>أعــداد : الطالب  / مجتبـــي كمال رشيد</h2>
+    <h2> أشــــرف  :  د /  محمـد عثمان</h2><marquee direction="up"scrollamount="2"><br><br><font size="100"><center><font color=' yellow'>{* كليـة الأمــــارات للعلـوم والتكنولوجيا *}</font c></font s>
+</marquee><br><p1><br></center>
+                           <center>
+    <img src="T11.png"width="760"hight="230><br>
+
+<br>     
+                      
+    <span><table border = 30 width="600"height="70"bgcolor="#090050"align="center"> 
+<tr>
+<td><center> <font color='yellow'><h1></center></td>
+</tr>   <span><table border = 30 width="600"height="70"bgcolor="#090050"align="center"> 
+<tr><br>
+<td><center> <font color='yellow'><h1>مـــــرحبآ</center></td>
+</tr>
+ <td>
+                                                                                      
+                                                                                      </center>
+                                                                                      <center><font color='yellow'>
+                                                                                      <h1>                                                                                      -: البيانات الأســاسية
+</h1> 
+                                                                                      
+<tr></tr>
+</tr> 
+<tr>
+<td><center> <font color='yellow'><h1></center></td>
+</tr> 
+                                                                                      
+    
+   <table border=  30 width="770"height="80"bgcolor="#5b0002"align="center">
+ 
+<caption/> <caption> 
+ <thead>
+ <tr>
+<th/><font color=' white'><h2>مجتبي كمال رشيد محمد</h2> <th> 
+ <th><font color='yellow'><h1>-: الأســم </h1></th>
+ </tr>
+ </thead> 
+<thead>
+ <tr>
+<th/><font color=' white'><h2>الخرطــوم / تـــــوتي</h2> <th> 
+ <th><font color='yellow'><h1> -: السكـن </h1></th>
+ </tr>
+ </thead>
+ <tr>
+<th/><font color=' white'><h2>95682</h2><th> 
+ <th><font color='yellow'><h1> -:  رقـم المنـزل </h1></th>
+ </tr>
+ </thead>
+     <thead>
+ <tr>
+<th/><font color=' white'><h2>0922951870</h2><th> 
+ <th><font color='yellow'><h1> -: رقـم الجـوال </h1></th>
+ </tr>
+ </thead>
+     <thead>
+ <tr>
+<th/><font color=' white'><h2>Mujtaba_459@outlok.com</h2><th> 
+ <th><font color='yellow'><h1>-:  البـريد الألكتـروني </h1></th>
+ </tr>
+ </thead>
+       <thead>
+ <tr>
+<th/><font color=' white'><h2>طالـــب جامعي</h2><th> 
+ <th><font color='yellow'><h1>-: الـوظيفـة </h1></th>
+ </tr>
+ </thead>
+       <thead>
+ <tr>
+<th/><font color=' white'><h2>كلية الأمارات للعلوم والتكنولوجيا</h2><th> 
+ <th><font color='yellow'><h1>-: أسـم الجــامعة </h1></th>
+ </tr>
+ </thead>
+       <thead>
+ <tr>
+<th/><font color=' white'><h2>[ iT ]  /  تقنيـة المعلـومــات </h2><th> 
+ <th><font color='yellow'><h1>-: التخـصص </h1></th>
+ </tr>
+ </thead>
+       <thead>
+ <tr>
+<th/><font color=' white'><h2>الخـــــامس</h2><th> 
+ <th><font color='yellow'><h1>-: الفصــل</h1></th>
+ </tr>
+ </thead>
+
+       
+<?php
+?>
+
+       <br>
+        
+</body>
+</html>
